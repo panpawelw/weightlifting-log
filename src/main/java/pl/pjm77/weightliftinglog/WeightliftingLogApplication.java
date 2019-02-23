0fd1a617ab38bd4e4efacea8443b30c9c4189847
Paweł Wojtuń <31450760+pjm77@users.noreply.github.com>
2019-02-24 00:00:14 +0100

    add initial configuration

---
+++ src/main/java/pl/pjm77/weightliftinglog/WeightliftingLogApplication.java
@@ -0,0 +1,13 @@
+package pl.pjm77.weightliftinglog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeightliftingLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeightliftingLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pjm77/weightliftinglog/WeightliftingLogApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.pjm77.weightliftinglog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WeightliftingLogApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
