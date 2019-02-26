dd722c869a40a0d3b876f05926a80414f5c7b4f7
Paweł Wojtuń <31450760+pjm77@users.noreply.github.com>
2019-02-26 14:07:23 +0100

    move SpringApplicationBuilder to WeightliftingLogApplication class to keep things tidy

--- src/main/java/pl/pjm77/weightliftinglog/WeightliftingLogApplication.java
+++ src/main/java/pl/pjm77/weightliftinglog/WeightliftingLogApplication.java
@@ -2,10 +2,16 @@ package pl.pjm77.weightliftinglog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
-public class WeightliftingLogApplication {
+@SpringBootApplication
+public class WeightliftingLogApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WeightliftingLogApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeightliftingLogApplication.class, args);
--- src/main/java/pl/pjm77/weightliftinglog/controllers/HomeController.java
+++ src/main/java/pl/pjm77/weightliftinglog/controllers/HomeController.java
@@ -2,18 +2,18 @@ package pl.pjm77.weightliftinglog.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/")
-    public String Home() {
+    public String home() {
         return "home";
     }
 
     @RequestMapping("/admin")
-    public String Admin() {
+    public String sdmin() {
         return "admin";
     }
 }
