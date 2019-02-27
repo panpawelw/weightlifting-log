adfcb488ec7735afc72fe66dc1bb41f21062a4d0
Paweł Wojtuń <31450760+pjm77@users.noreply.github.com>
2019-02-27 14:47:59 +0100

    add Role entity

--- src/main/java/pl/pjm77/weightliftinglog/models/Role.java
+++ src/main/java/pl/pjm77/weightliftinglog/models/Role.java
@@ -4,25 +4,22 @@ import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 
 @Entity
-@Table(name="users")
-public class User {
+@Table(name="roles")
+public class Role {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer Id;
 
     @NotBlank
     private String name;
 
-    @NotBlank
-    private String password;
-
-    public Long getId() {
-        return id;
+    public Integer getId() {
+        return Id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setId(Integer id) {
+        Id = id;
     }
 
     public String getName() {
@@ -31,13 +28,5 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 }
