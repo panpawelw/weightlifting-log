5aac9a1e606f4e44ad59cc5014c0d4689b10de50
Paweł Wojtuń <31450760+pjm77@users.noreply.github.com>
2019-03-01 13:57:57 +0100

    add http security configuration to Spring Security configuration

--- src/main/java/pl/pjm77/weightliftinglog/config/SecurityConfiguration.java
+++ src/main/java/pl/pjm77/weightliftinglog/config/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -25,6 +26,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(myUserDetailsService).passwordEncoder(getPasswordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers(("/**")).authenticated()
+                .anyRequest().permitAll().and().formLogin().permitAll();
     }
 
     private PasswordEncoder getPasswordEncoder() {
