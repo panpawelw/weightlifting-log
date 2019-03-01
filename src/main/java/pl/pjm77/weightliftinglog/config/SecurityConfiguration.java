060d1952dfc4b2cad892d3c33855df70814b5346
Paweł Wojtuń <31450760+pjm77@users.noreply.github.com>
2019-03-01 13:28:28 +0100

    add mock-up password encoder to Spring Security configuration

--- src/main/java/pl/pjm77/weightliftinglog/config/SecurityConfiguration.java
+++ src/main/java/pl/pjm77/weightliftinglog/config/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import pl.pjm77.weightliftinglog.repositories.UserRepository;
 import pl.pjm77.weightliftinglog.services.MyUserDetailsService;
 
@@ -23,6 +24,20 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(myUserDetailsService);
+        auth.userDetailsService(myUserDetailsService).passwordEncoder(getPasswordEncoder());
+    }
+
+    private PasswordEncoder getPasswordEncoder() {
+        return new PasswordEncoder() {
+            @Override
+            public String encode(CharSequence charSequence) {
+                return charSequence.toString();
+            }
+
+            @Override
+            public boolean matches(CharSequence charSequence, String s) {
+                return false;
+            }
+        };
     }
 }
