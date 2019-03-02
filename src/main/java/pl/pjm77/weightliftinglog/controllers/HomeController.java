12e65bad22b1ec5721c2c84787138c9f228c3777
Paweł Wojtuń <31450760+pjm77@users.noreply.github.com>
2019-03-02 14:34:53 +0100

    begin app homepage construction

--- src/main/java/pl/pjm77/weightliftinglog/controllers/HomeController.java
+++ src/main/java/pl/pjm77/weightliftinglog/controllers/HomeController.java
@@ -2,14 +2,20 @@ package pl.pjm77.weightliftinglog.controllers;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-//    @PreAuthorize("hasAnyRole('USER')")
+    //    @PreAuthorize("hasAnyRole('USER')")
     @RequestMapping("/")
     public String home() {
+        return "home";
+    }
+
+    @PostMapping("/")
+    public String loggedIn() {
         return "home";
     }
 
