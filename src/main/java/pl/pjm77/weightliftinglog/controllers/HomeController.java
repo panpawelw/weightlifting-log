a2311c8710cb84f6de1cbea7b5aa898675e23d6c
Paweł Wojtuń <31450760+pjm77@users.noreply.github.com>
2019-03-10 22:45:20 +0100

    add home page main content swapping using Thymeleaf fragments

--- src/main/java/pl/pjm77/weightliftinglog/controllers/HomeController.java
+++ src/main/java/pl/pjm77/weightliftinglog/controllers/HomeController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 @Controller
 public class HomeController {
 
@@ -48,16 +51,17 @@ public class HomeController {
     @PreAuthorize("hasAnyRole('USER','ADMIN')")
     @RequestMapping("/user")
     public String user(Model model) {
-        System.out.println("user");
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         String username;
+        Collection authorities = new ArrayList<>();
         if (principal instanceof UserDetails) {
             username = ((UserDetails)principal).getUsername();
+            authorities =((UserDetails)principal).getAuthorities();
         } else {
             username = principal.toString();
         }
         model.addAttribute("userIndicator", "This is " + username + " logged in!");
-        System.out.println(((UserDetails) principal).getAuthorities());
+        System.out.println(authorities);
         return "home";
     }
 
