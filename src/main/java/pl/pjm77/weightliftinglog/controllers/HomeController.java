1eee044fd8dbec749d2774b8ed81e4010a868487
Paweł Wojtuń <31450760+pjm77@users.noreply.github.com>
2019-03-12 01:38:27 +0100

    add "log out" (not functional yet) and optional "admin panel" buttons to user panel

--- src/main/java/pl/pjm77/weightliftinglog/controllers/HomeController.java
+++ src/main/java/pl/pjm77/weightliftinglog/controllers/HomeController.java
@@ -1,6 +1,8 @@
 package pl.pjm77.weightliftinglog.controllers;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -8,12 +10,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.pjm77.weightliftinglog.models.User;
+import pl.pjm77.weightliftinglog.services.MyUserDetailsService;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Optional;
 
 @Controller
 public class HomeController {
+
+    private MyUserDetailsService myUserDetailsService;
 
     @GetMapping("/")
     public String home(Model model) {
@@ -27,7 +34,7 @@ public class HomeController {
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         if (principal instanceof UserDetails) {
             return "redirect:/user";
-        }else {
+        } else {
             model.addAttribute("page", "fragments.html :: login");
             return "home";
         }
@@ -62,12 +69,17 @@ public class HomeController {
     public String user(Model model) {
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         String username;
-        Collection authorities = new ArrayList<>();
+        Collection<? extends GrantedAuthority> authorities = new ArrayList<>();
         if (principal instanceof UserDetails) {
-            username = ((UserDetails)principal).getUsername();
-            authorities =((UserDetails)principal).getAuthorities();
+            username = ((UserDetails) principal).getUsername();
+            authorities = ((UserDetails) principal).getAuthorities();
         } else {
             username = principal.toString();
+        }
+        for (GrantedAuthority grantedAuthority : authorities) {
+            if (("ROLE_ADMIN").equals(grantedAuthority.getAuthority())) {
+                model.addAttribute("adminRights", true);
+            }
         }
         model.addAttribute("userIndicator", "Hello " + username + "!");
         model.addAttribute("page", "fragments.html :: userpanel");
@@ -77,7 +89,7 @@ public class HomeController {
     @PreAuthorize("hasAnyRole('ADMIN')")
     @RequestMapping("/admin")
     public String admin(Model model) {
-        model.addAttribute("page", "fragments.html :: admin");
+        model.addAttribute("page", "fragments.html :: adminpanel");
         return "home";
     }
 }
