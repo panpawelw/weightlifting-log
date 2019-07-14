4e627c0d8016782752569dc0d34c8556c7da4015
Paweł Wojtuń <31450760+pjm77@users.noreply.github.com>
2019-07-14 18:16:52 +0200

    add workout serialization / deserialization

--- src/main/java/pl/pjm77/weightliftinglog/models/WorkoutDeserialized.java
+++ src/main/java/pl/pjm77/weightliftinglog/models/WorkoutDeserialized.java
@@ -11,6 +11,76 @@ public class WorkoutDeserialized implements Serializable {
     private String title;
     private Timestamp created;
     private Timestamp updated;
+    private User user;
     private List<Exercise> exercises = new ArrayList<>();
-    private List<Note> workoutNotes = new ArrayList();
+    private List<Note> workoutNotes = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Timestamp getCreated() {
+        return created;
+    }
+
+    public void setCreated(Timestamp created) {
+        this.created = created;
+    }
+
+    public Timestamp getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(Timestamp updated) {
+        this.updated = updated;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<Exercise> getExercises() {
+        return exercises;
+    }
+
+    public void setExercises(List<Exercise> exercises) {
+        this.exercises = exercises;
+    }
+
+    public List<Note> getWorkoutNotes() {
+        return workoutNotes;
+    }
+
+    public void setWorkoutNotes(List<Note> workoutNotes) {
+        this.workoutNotes = workoutNotes;
+    }
+
+    @Override
+    public String toString() {
+        return "WorkoutDeserialized{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", created=" + created +
+                ", updated=" + updated +
+                ", user=" + user +
+                ", exercises=" + exercises +
+                ", workoutNotes=" + workoutNotes +
+                '}';
+    }
 }
--- src/main/java/pl/pjm77/weightliftinglog/services/WorkoutService.java
+++ src/main/java/pl/pjm77/weightliftinglog/services/WorkoutService.java
@@ -2,7 +2,11 @@ package pl.pjm77.weightliftinglog.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.pjm77.weightliftinglog.models.WorkoutDeserialized;
+import pl.pjm77.weightliftinglog.models.WorkoutSerialized;
 import pl.pjm77.weightliftinglog.repositories.WorkoutRepository;
+
+import java.io.*;
 
 @Service
 public class WorkoutService {
@@ -12,5 +16,55 @@ public class WorkoutService {
     @Autowired
     public WorkoutService(WorkoutRepository workoutRepository) {
         this.workoutRepository = workoutRepository;
+    }
+
+    public WorkoutSerialized serializeWorkout
+            (WorkoutDeserialized workoutDeserialized) {
+        WorkoutSerialized workoutSerialized = new WorkoutSerialized();
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutput out;
+        try {
+            out = new ObjectOutputStream(bos);
+            out.writeObject(workoutDeserialized);
+            out.flush();
+            workoutSerialized.setData(bos.toByteArray());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                bos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        workoutSerialized.setId(workoutDeserialized.getId());
+        workoutSerialized.setTitle(workoutDeserialized.getTitle());
+        workoutSerialized.setCreated(workoutDeserialized.getCreated());
+        workoutSerialized.setUpdated(workoutDeserialized.getUpdated());
+        workoutSerialized.setUser(workoutDeserialized.getUser());
+        return workoutSerialized;
+    }
+
+    public WorkoutDeserialized deserializeWorkout
+            (WorkoutSerialized workoutSerialized) {
+        WorkoutDeserialized workoutDeserialized = new WorkoutDeserialized();
+        ByteArrayInputStream bis = new ByteArrayInputStream(workoutSerialized.getData());
+        ObjectInput in = null;
+        try {
+            in = new ObjectInputStream(bis);
+            Object o = in.readObject();
+            workoutDeserialized = (WorkoutDeserialized) o;
+        } catch (IOException | ClassNotFoundException e){
+            e.printStackTrace();
+        } finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return workoutDeserialized;
     }
 }
