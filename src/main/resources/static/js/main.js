a2cda08be0b44ee86fe6ef23c0edcdd92caf06a6
Paweł Wojtuń <31450760+pjm77@users.noreply.github.com>
2019-07-27 22:59:46 +0200

    Add text field to percentages table

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -56,32 +56,32 @@ function calculate1RM() {
     let reps = document.getElementById('reps-text').value;
     let result = weight * (1 + (reps / 30));
     document.getElementById('result').value = result;
-    document.getElementById('percentage-1').innerText =
-        (document.getElementById('percentage-input-1').value * 0.01 * result).toFixed(1);
-    document.getElementById('percentage-2').innerText =
-        (document.getElementById('percentage-input-2').value * 0.01 * result).toFixed(1);
-    document.getElementById('percentage-3').innerText =
-        (document.getElementById('percentage-input-3').value * 0.01 * result).toFixed(1);
-    document.getElementById('percentage-4').innerText =
-        (document.getElementById('percentage-input-4').value * 0.01 * result).toFixed(1);
-    document.getElementById('percentage-5').innerText =
-        (document.getElementById('percentage-input-5').value * 0.01 * result).toFixed(1);
-    document.getElementById('percentage-6').innerText =
-        (document.getElementById('percentage-input-6').value * 0.01 * result).toFixed(1);
-    document.getElementById('percentage-7').innerText =
-        (document.getElementById('percentage-input-7').value * 0.01 * result).toFixed(1);
-    document.getElementById('percentage-8').innerText =
-        (document.getElementById('percentage-input-8').value * 0.01 * result).toFixed(1);
-    document.getElementById('percentage-9').innerText =
-        (document.getElementById('percentage-input-9').value * 0.01 * result).toFixed(1);
-    document.getElementById('percentage-10').innerText =
-        (document.getElementById('percentage-input-10').value * 0.01 * result).toFixed(1);
-    document.getElementById('percentage-11').innerText =
-        (document.getElementById('percentage-input-11').value * 0.01 * result).toFixed(1);
-    document.getElementById('percentage-12').innerText =
-        (document.getElementById('percentage-input-12').value * 0.01 * result).toFixed(1);
-    document.getElementById('percentage-13').innerText =
-        (document.getElementById('percentage-input-13').value * 0.01 * result).toFixed(1);
+    document.getElementById('percentage-1').value =
+        (document.getElementById('percentage-input-1').value * 0.01 * result).toFixed(2);
+    document.getElementById('percentage-2').value =
+        (document.getElementById('percentage-input-2').value * 0.01 * result).toFixed(2);
+    document.getElementById('percentage-3').value =
+        (document.getElementById('percentage-input-3').value * 0.01 * result).toFixed(2);
+    document.getElementById('percentage-4').value =
+        (document.getElementById('percentage-input-4').value * 0.01 * result).toFixed(2);
+    document.getElementById('percentage-5').value =
+        (document.getElementById('percentage-input-5').value * 0.01 * result).toFixed(2);
+    document.getElementById('percentage-6').value =
+        (document.getElementById('percentage-input-6').value * 0.01 * result).toFixed(2);
+    document.getElementById('percentage-7').value =
+        (document.getElementById('percentage-input-7').value * 0.01 * result).toFixed(2);
+    document.getElementById('percentage-8').value =
+        (document.getElementById('percentage-input-8').value * 0.01 * result).toFixed(2);
+    document.getElementById('percentage-9').value =
+        (document.getElementById('percentage-input-9').value * 0.01 * result).toFixed(2);
+    document.getElementById('percentage-10').value =
+        (document.getElementById('percentage-input-10').value * 0.01 * result).toFixed(2);
+    document.getElementById('percentage-11').value =
+        (document.getElementById('percentage-input-11').value * 0.01 * result).toFixed(2);
+    document.getElementById('percentage-12').value =
+        (document.getElementById('percentage-input-12').value * 0.01 * result).toFixed(2);
+    document.getElementById('percentage-13').value =
+        (document.getElementById('percentage-input-13').value * 0.01 * result).toFixed(2);
 }
 
 /* This function updates an element of certain id with given value */
