9cb6400bfead351a60a6bf377b37b08918a97366
Paweł Wojtuń <31450760+pjm77@users.noreply.github.com>
2019-08-07 16:51:57 +0200

    Add password validation annotation

--- src/main/java/pl/pjm77/weightliftinglog/security/SecurityConfiguration.java
+++ src/main/java/pl/pjm77/weightliftinglog/security/SecurityConfiguration.java
@@ -1,4 +1,4 @@
-package pl.pjm77.weightliftinglog.config;
+package pl.pjm77.weightliftinglog.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
