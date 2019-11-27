8029644bff7ef53441ab713d0d1f44b2f4b6aef5
Paweł Wojtuń <31450760+pjm77@users.noreply.github.com>
2019-11-27 23:52:11 +0100

    Add Spring Mail dependency and Email service

---
+++ src/main/java/pl/pjm77/weightliftinglog/services/EmailService.java
@@ -0,0 +1,25 @@
+package pl.pjm77.weightliftinglog.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    private JavaMailSender javaMailSender;
+
+    @Autowired
+    public EmailService (JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    public void sendEmail (String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        javaMailSender.send(message);
+    }
+}
