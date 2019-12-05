c0b0435bd4ce96b483380b749bedebfbf74ad13c
Paweł Wojtuń <31450760+pjm77@users.noreply.github.com>
2019-12-05 02:31:39 +0100

    Add more tests to MockMvcTests

--- src/test/java/pl/pjm77/weightliftinglog/MockMvcTests.java
+++ src/test/java/pl/pjm77/weightliftinglog/MockMvcTests.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.stereotype.Controller;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -33,6 +36,20 @@ public class MockMvcTests {
                 .string(containsString("everybody")));
     }
 
+    @Test
+    public void userAttemptsAdmin() throws Exception {
+        this.mockMvc.perform(get("/wl/admin")).andExpect(status().isUnauthorized());
+    }
+
+    @Configuration
+    public static class MyTestConfiguration extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests().antMatchers("wl/admin/**").hasRole("USER").and()
+                    .httpBasic();
+        }
+    }
+
     @Controller
     @TestConfiguration
     public static class TestController {
@@ -40,6 +57,16 @@ public class MockMvcTests {
         @RequestMapping(path = "/wl")
         public ResponseEntity<String> everybody() {
             return ResponseEntity.ok("everybody");
+        }
+
+        @RequestMapping(path = "wl/user")
+        public ResponseEntity<String> user() {
+            return ResponseEntity.ok("user");
+        }
+
+        @RequestMapping(path = "wl/admin")
+        public ResponseEntity<String> admin() {
+            return ResponseEntity.ok("admin");
         }
     }
 }
