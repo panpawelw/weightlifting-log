9ecb6a16ab6f5cb669176761d6869869186a5c6d
Paweł Wojtuń <31450760+pjm77@users.noreply.github.com>
2020-01-31 18:55:30 +0100

    Merge ASW configuration and service for simplicity

--- src/main/java/pl/pjm77/weightliftinglog/services/S3FileService.java
+++ src/main/java/pl/pjm77/weightliftinglog/services/S3FileService.java
@@ -1,34 +1,58 @@
 package pl.pjm77.weightliftinglog.services;
 
 import com.amazonaws.AmazonClientException;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.util.IOUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import pl.pjm77.weightliftinglog.AWS.AmazonS3Configuration;
 import pl.pjm77.weightliftinglog.models.MediaFile;
 import pl.pjm77.weightliftinglog.models.WorkoutDeserialized;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.util.List;
 
 @Service
 public class S3FileService {
 
+    private AmazonS3 amazonS3Client;
+
+    @Value("${cloud.aws.credentials.accessKey}")
+    private String awsKeyId;
+
+    @Value("${cloud.aws.credentials.secretKey}")
+    private String awsKeySecret;
+
+    @Value("${cloud.aws.region.static}")
+    private String awsRegion;
+
+    @PostConstruct
+    private void initializeAmazon() {
+        BasicAWSCredentials credentialsProvider =
+                new BasicAWSCredentials(awsKeyId,awsKeySecret);
+        this.amazonS3Client = AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentialsProvider))
+                .withRegion(Regions.fromName(awsRegion))
+                .build();
+    }
+
     private final String bucketName = "weightliftinglogbucket";
-    private final AmazonS3Configuration amazonS3Configuration;
     private final WorkoutService workoutService;
 
-    public S3FileService(AmazonS3Configuration amazonS3Configuration, WorkoutService workoutService) {
-        this.amazonS3Configuration = amazonS3Configuration;
+    public S3FileService(WorkoutService workoutService) {
         this.workoutService = workoutService;
     }
 
     public void StoreAllFiles(WorkoutDeserialized workout,
                               MultipartFile[] files) {
-        AmazonS3 amazonS3Client = amazonS3Configuration.amazonS3Client();
         List<String> filenames = workout.getFilenames();
         String filename;
         for (MultipartFile file : files) {
@@ -48,7 +72,6 @@ public class S3FileService {
     }
 
     public MediaFile getFileByWorkoutIdAndFilename(Long workoutId, String filename) {
-        AmazonS3 amazonS3Client = amazonS3Configuration.amazonS3Client();
         S3Object s3Object;
         ObjectMetadata objectMetadata;
         byte[] content;
@@ -65,7 +88,6 @@ public class S3FileService {
     }
 
     public void deleteFileByWorkoutAndFilename(WorkoutDeserialized workout, String filename) {
-        AmazonS3 amazonS3Client = amazonS3Configuration.amazonS3Client();
         filename = workout.getId() + "\\" + filename;
         try {
             amazonS3Client.deleteObject(bucketName, filename);
@@ -75,7 +97,6 @@ public class S3FileService {
     }
 
     public void deleteAllByWorkoutId(long workoutId) {
-        AmazonS3 amazonS3Client = amazonS3Configuration.amazonS3Client();
         List<String> filesToDelete = workoutService.findWorkoutById(workoutId).getFilenames();
         filesToDelete.forEach(filename -> {
             try {
