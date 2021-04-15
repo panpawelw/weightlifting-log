fb0d5266785f601918b4d527ce1b4038a08e9fac
Paweł Wojtuń <panpawelw@pm.me>
2021-04-15 18:24:46 +0200

    Add VerificationTokenServiceTests stub

---
+++ src/test/java/com/panpawelw/weightliftinglog/servicestests/VerificationTokenServiceTests.java
@@ -0,0 +1,31 @@
+package com.panpawelw.weightliftinglog.servicestests;
+
+import org.junit.Test;
+
+public class VerificationTokenServiceTests {
+
+  @Test
+  public void testFindByUser() {
+
+  }
+
+  @Test
+  public void testFindByToken() {
+
+  }
+
+  @Test
+  public void testSaveToken() {
+
+  }
+
+  @Test
+  public void testDeleteVerificationToken() {
+
+  }
+
+  @Test
+  public void testRemoveAccountIfTokenExpired() {
+
+  }
+}
