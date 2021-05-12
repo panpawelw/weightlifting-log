2078d7c0630891933bf850c03a6a48f5185f02ff
Paweł Wojtuń <panpawelw@pm.me>
2021-05-12 16:18:22 +0200

    Add DBFileServiceTests

---
+++ src/test/java/com/panpawelw/weightliftinglog/servicestests/DBFileServiceTests.java
@@ -0,0 +1,85 @@
+package com.panpawelw.weightliftinglog.servicestests;
+
+import com.panpawelw.weightliftinglog.models.User;
+import com.panpawelw.weightliftinglog.models.WorkoutDeserialized;
+import com.panpawelw.weightliftinglog.repositories.FileRepository;
+import com.panpawelw.weightliftinglog.services.DBFileService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DBFileServiceTests {
+
+  private static final WorkoutDeserialized TEST_WORKOUT = new WorkoutDeserialized(1L,
+      "Test title", null, null, new User(), new ArrayList<>(), new ArrayList<>(),
+      new ArrayList<>(Arrays.asList("audio_file.mp3", "photo.jpg", "video_clip.mp4")));
+
+  private static final MultipartFile[] TEST_WORKOUT_FILES = new MultipartFile[]{
+      new MockMultipartFile("testaudio.mp3", "testaudio.mp3",
+          "audio/mpeg", new byte[]{110, (byte) 160, 7, 47, 49, 24, 41, 113, 103, 123}),
+      new MockMultipartFile("testimage.bmp", "testimage.bmp",
+          "image/bmp", new byte[]{41, 91, 115, 16, 22, 118, 122, 49,  28, 97}),
+      new MockMultipartFile("testvideo.mp4", "testvideo.mp4",
+          "video/mp4", new byte[]{113, 17, 78, 6, 89, 24, 23, (byte) 199, 83, 22}),
+  };
+
+  @Mock
+  private FileRepository repository;
+
+  private DBFileService service;
+
+  @Before
+  public void setup() {
+    this.service = new DBFileService(repository);
+  }
+
+  @Test
+  public void testStoreAllFilesByWorkout() {
+
+  }
+
+  @Test
+  public void testStoreAllFilesByWorkoutWithIncorrectParameters() {
+
+  }
+
+  @Test
+  public void testGetFileByWorkoutIdAndFilename() {
+
+  }
+
+  @Test
+  public void testGetFileByWorkoutIdAndFilenameWithIncorrectParameters() {
+
+  }
+
+  @Test
+  public void testDeleteFileByWorkoutAndFilename() {
+
+  }
+
+  @Test
+  public void testDeleteFileByWorkoutAndFilenameWithIncorrectParameters() {
+
+  }
+
+  @Test
+  public void testDeleteAllFilesByWorkoutId() {
+    service.deleteAllFilesByWorkoutId(1);
+    verify(repository).deleteAllByWorkoutId(1L);
+  }
+
+  @Test
+  public void testDeleteAllFilesByWorkoutIdWithIncorrectParameters() {
+  }
+}
