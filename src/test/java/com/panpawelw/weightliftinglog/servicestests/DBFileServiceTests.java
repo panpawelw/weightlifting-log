4951fe9ba0e7a55010329e8c970e073e9e749fcc
Paweł Wojtuń <panpawelw@pm.me>
2021-05-14 00:00:22 +0200

    Add 2 tests:
    -add testStoreAllFilesByWorkout()
    -add testGetFileByWorkoutIdAndFilename()

--- src/test/java/com/panpawelw/weightliftinglog/servicestests/DBFileServiceTests.java
+++ src/test/java/com/panpawelw/weightliftinglog/servicestests/DBFileServiceTests.java
@@ -1,5 +1,6 @@
 package com.panpawelw.weightliftinglog.servicestests;
 
+import com.panpawelw.weightliftinglog.models.MediaFile;
 import com.panpawelw.weightliftinglog.models.User;
 import com.panpawelw.weightliftinglog.models.WorkoutDeserialized;
 import com.panpawelw.weightliftinglog.repositories.FileRepository;
@@ -15,7 +16,11 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class DBFileServiceTests {
@@ -45,7 +50,12 @@ public class DBFileServiceTests {
 
   @Test
   public void testStoreAllFilesByWorkout() {
+    service.storeAllFilesByWorkout(TEST_WORKOUT, TEST_WORKOUT_FILES);
 
+    verify(repository).saveAll(any());
+    verify(repository).flush();
+    assertTrue(TEST_WORKOUT.getFilenames().containsAll(new ArrayList<>(
+        Arrays.asList("testaudio.mp3", "testimage.bmp", "testvideo.mp4"))));
   }
 
   @Test
@@ -55,7 +65,12 @@ public class DBFileServiceTests {
 
   @Test
   public void testGetFileByWorkoutIdAndFilename() {
+    MediaFile testFile = new MediaFile(null, 3L, "audio_file.mp3", "audio/mpeg",
+        new byte[]{69, 121, 101, 45, 62, 118, 101, 114, (byte) 196, (byte) 195, 61, 101, 98});
+    when(repository.findFileByWorkoutIdAndFilename(1L, "audio_file.mp3")).thenReturn(testFile);
 
+    assertEquals(service.getFileByWorkoutIdAndFilename(1L, "audio_file.mp3"), testFile);
+    verify(repository).findFileByWorkoutIdAndFilename(1L, "audio_file.mp3");
   }
 
   @Test
