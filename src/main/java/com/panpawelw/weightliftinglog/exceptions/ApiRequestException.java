bae64c1e6ff7da117385764c87d013261b711e19
Paweł Wojtuń <panpawelw@pm.me>
2021-05-19 20:05:03 +0200

    Add custom ApiRequestException

---
+++ src/main/java/com/panpawelw/weightliftinglog/exceptions/ApiRequestException.java
@@ -0,0 +1,12 @@
+package com.panpawelw.weightliftinglog.exceptions;
+
+public class ApiRequestException extends RuntimeException {
+
+  public ApiRequestException(String message) {
+    super(message);
+  }
+
+  public ApiRequestException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
---
+++ src/main/java/com/panpawelw/weightliftinglog/exceptions/ApiRequestExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.panpawelw.weightliftinglog.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+@ControllerAdvice
+public class ApiRequestExceptionHandler {
+
+  @ExceptionHandler(value = {ApiRequestException.class})
+  public ResponseEntity<Object> handleApiRequestException(ApiRequestException e) {
+    ApiRequestExceptionObject apiRequestExceptionObject = new ApiRequestExceptionObject(
+        e.getMessage(), e, HttpStatus.BAD_REQUEST, ZonedDateTime.now(ZoneId.of("Europe/Warsaw")));
+    return new ResponseEntity<>(apiRequestExceptionObject, HttpStatus.BAD_REQUEST);
+  }
+}
