40f28b23768538b2274d4df30c64b0305e6bdadd
Paweł Wojtuń <panpawelw@pm.me>
2021-06-23 00:08:56 +0200

    Modify WeightliftingLogApplicationTest

--- src/test/java/com/panpawelw/weightliftinglog/weblayertests/WeightliftingLogApplicationTest.java
+++ src/test/java/com/panpawelw/weightliftinglog/weblayertests/WeightliftingLogApplicationTest.java
@@ -5,15 +5,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.stereotype.Controller;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -30,44 +23,8 @@ public class WeightliftingLogApplicationTest {
   private MockMvc mockMvc;
 
   @Test
-  public void testRegularUser() throws Exception {
-    this.mockMvc.perform(get("/weightliftinglog")).andDo(print())
-        .andExpect(status().isOk()).andExpect(content()
-        .string(containsString("everybody")));
-  }
-
-  @Test
-  public void testUserAttemptsAdmin() throws Exception {
-    this.mockMvc.perform(get("/weightliftinglog/admin"))
-        .andExpect(status().isUnauthorized());
-  }
-
-  @Configuration
-  public static class MyTestConfiguration extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-      http.authorizeRequests().antMatchers("/weightliftinglog/admin/**")
-          .hasRole("USER").and().httpBasic();
-    }
-  }
-
-  @Controller
-  @TestConfiguration
-  public static class TestController {
-
-    @RequestMapping(path = "/weightliftinglog")
-    public ResponseEntity<String> everybody() {
-      return ResponseEntity.ok("everybody");
-    }
-
-    @RequestMapping(path = "/weightliftinglog/user")
-    public ResponseEntity<String> user() {
-      return ResponseEntity.ok("user");
-    }
-
-    @RequestMapping(path = "/weightliftinglog/admin")
-    public ResponseEntity<String> admin() {
-      return ResponseEntity.ok("admin");
-    }
+  public void shouldReturnDefaultMessage() throws Exception {
+    this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+        .andExpect(content().string(containsString("Weightlifting Log")));
   }
 }
