9dc86d2e366d683f268764020f87b710e3b25eb3
Paweł Wojtuń <panpawelw@pm.me>
2021-09-04 01:50:04 +0200

    Improve testChangePasswordGet()

--- src/test/java/com/panpawelw/weightliftinglog/controllertests/ChangePasswordControllerTests.java
+++ src/test/java/com/panpawelw/weightliftinglog/controllertests/ChangePasswordControllerTests.java
@@ -2,37 +2,40 @@ package com.panpawelw.weightliftinglog.controllertests;
 
 import com.panpawelw.weightliftinglog.controllers.ChangePasswordController;
 import com.panpawelw.weightliftinglog.services.UserService;
-import com.panpawelw.weightliftinglog.validators.ChangePasswordValidator;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.ui.Model;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(MockitoJUnitRunner.class)
+@WebMvcTest(controllers = ChangePasswordController.class)
 public class ChangePasswordControllerTests {
+
+  @Autowired
+  private MockMvc mockMvc;
 
   @Mock
   private UserService service;
 
-  @Mock
-  private ChangePasswordValidator validator;
-
-  @Mock
-  private Model model;
-
-  private ChangePasswordController controller;
-
   @Before
   public void setup() {
-    this.controller = new ChangePasswordController(validator, service);
+    ChangePasswordController controller = new ChangePasswordController(null, service);
+    this.mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
   }
 
   @Test
-  public void testChangePasswordGet() {
-    assertEquals(controller.changePasswordGet(model), "home");
+  public void testChangePasswordGet() throws Exception {
+    mockMvc.perform(get("/user/changepassword"))
+        .andExpect(status().isOk())
+        .andExpect(forwardedUrl("home"))
+        .andExpect(model().attribute("page", "fragments.html :: change-password"));
   }
 }
