a3092459a4b9d773794345e8b89b097b6e89ee0d
Paweł Wojtuń <panpawelw@pm.me>
2021-10-26 00:27:53 +0200

    Add ShouldReturnUserPanel()

--- src/test/java/com/panpawelw/weightliftinglog/controllertests/UserControllerTests.java
+++ src/test/java/com/panpawelw/weightliftinglog/controllertests/UserControllerTests.java
@@ -6,6 +6,7 @@ import com.panpawelw.weightliftinglog.services.VerificationTokenService;
 import com.panpawelw.weightliftinglog.services.WorkoutService;
 import com.panpawelw.weightliftinglog.validators.UpdateDetailsPasswordValidator;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
@@ -13,6 +14,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static com.panpawelw.weightliftinglog.constants.TEST_USER;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 @WebMvcTest(controllers = UserController.class)
@@ -38,5 +45,22 @@ public class UserControllerTests {
     UserController controller = new UserController(
         service, workoutService, validator, verificationTokenService);
     this.mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+  }
+
+  @Test
+  public void shouldReturnUserPanel() throws Exception {
+    when(service.getLoggedInUsersEmail()).thenReturn(TEST_USER.getEmail());
+    when(service.findUserByEmail(TEST_USER.getEmail())).thenReturn(TEST_USER);
+    when(service.checkLoggedInUserForAdminRights()).thenReturn(true);
+
+    mockMvc.perform(get("/user"))
+        .andExpect(status().isOk())
+        .andExpect(model().attribute("userName", TEST_USER.getName()))
+        .andExpect(model().attribute("adminRights", true))
+        .andExpect(model().attribute("page", "fragments.html :: user-panel"))
+        .andExpect(model().attribute("userPanelPage", "fragments.html :: user-panel-default"))
+        .andExpect(model().attribute("workouts", TEST_USER.getWorkouts()));
+    verify(service).findUserByEmail(TEST_USER.getEmail());
+    verify(service).checkLoggedInUserForAdminRights();
   }
 }
