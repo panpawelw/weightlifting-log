c5226dc7014ba2f127df4136b1dc8f4f441bdc51
Paweł Wojtuń <panpawelw@pm.me>
2021-11-17 23:59:04 +0100

    Add populateDatabase()

--- src/test/java/com/panpawelw/weightliftinglog/serviceIT/UserServiceIT.java
+++ src/test/java/com/panpawelw/weightliftinglog/serviceIT/UserServiceIT.java
@@ -1,5 +1,6 @@
 package com.panpawelw.weightliftinglog.serviceIT;
 
+import com.panpawelw.weightliftinglog.models.User;
 import com.panpawelw.weightliftinglog.services.UserService;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
 
 import static com.panpawelw.weightliftinglog.constants.TEST_USER;
 import static org.junit.Assert.assertEquals;
@@ -23,6 +26,7 @@ public class UserServiceIT {
   @Before
   public void setup() {
     service.saveUser(TEST_USER);
+    populateDatabase();
   }
 
   @Test
@@ -45,5 +49,15 @@ public class UserServiceIT {
   @Test(expected = EmptyResultDataAccessException.class)
   public void deleteUserByIdShouldFail() {
     service.deleteUserById(100);
+  }
+
+  void populateDatabase() {
+    for (Long i=1L; i<5; i++) {
+      String suffix = String.valueOf(i);
+      User user = new User(i, "Test name" + suffix, "Test password" + suffix,
+          "Test password" + suffix, "test@email" + suffix, true, "Test First Name" + suffix,
+          "Test Last Name" + suffix, i.intValue(), true, "USER", new ArrayList<>());
+      service.saveUser(user);
+    }
   }
 }
