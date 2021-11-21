206eb2d97ed5f3f3471bd3b8debfde639830933b
Paweł Wojtuń <panpawelw@pm.me>
2021-11-21 23:27:50 +0100

    Add sql data, remove populateDatabase()

--- src/test/java/com/panpawelw/weightliftinglog/serviceIT/UserServiceIT.java
+++ src/test/java/com/panpawelw/weightliftinglog/serviceIT/UserServiceIT.java
@@ -2,7 +2,6 @@ package com.panpawelw.weightliftinglog.serviceIT;
 
 import com.panpawelw.weightliftinglog.models.User;
 import com.panpawelw.weightliftinglog.services.UserService;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mindrot.jbcrypt.BCrypt;
@@ -11,8 +10,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.ArrayList;
 
 import static com.panpawelw.weightliftinglog.constants.TEST_USER;
 import static org.junit.Assert.*;
@@ -23,11 +20,6 @@ public class UserServiceIT {
 
   @Autowired
   private UserService service;
-
-  @Before
-  public void setup() {
-    populateDatabase();
-  }
 
   @Test
   public void findUserByEmailShouldReturnUser() {
@@ -59,8 +51,9 @@ public class UserServiceIT {
     try {
       service.saveUser(user);
     } catch (DataIntegrityViolationException e) {
-      assertEquals(databaseCount, service.count());
       assertTrue(e.getMostSpecificCause().getMessage().contains("PUBLIC.USER_UNIQUE_NAME"));
+    } finally {
+      assertEquals(databaseCount, service.count());
     }
   }
 
@@ -73,8 +66,9 @@ public class UserServiceIT {
     try {
       service.saveUser(user);
     } catch (DataIntegrityViolationException e) {
-      assertEquals(databaseCount, service.count());
       assertTrue(e.getMostSpecificCause().getMessage().contains("PUBLIC.USER_UNIQUE_EMAIL"));
+    } finally {
+      assertEquals(databaseCount, service.count());
     }
   }
 
@@ -88,16 +82,5 @@ public class UserServiceIT {
   @Test(expected = EmptyResultDataAccessException.class)
   public void deleteUserByIdShouldFail() {
     service.deleteUserById(100);
-  }
-
-  void populateDatabase() {
-    for (Long i = 1L; i < 6; i++) {
-      String suffix = String.valueOf(i);
-      User user = new User(i, "Test name" + suffix, "Test password" + suffix,
-          "Test password" + suffix, "test@email" + suffix + ".com", true,
-          "Test First Name" + suffix, "Test Last Name" + suffix, i.intValue(),
-          true, "USER", new ArrayList<>());
-      service.saveUser(user);
-    }
   }
 }
---
+++ src/test/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO USERS (id, name, password, email, activated, first_name, last_name, age, gender, role) VALUES
+(1, 'Test name1', 'Test password1', 'test@email1.com', 1, 'Test first name1', 'Test last name1', 1, 1, 'USER'),
+(2, 'Test name2', 'Test password2', 'test@email2.com', 1, 'Test first name2', 'Test last name2', 2, 1, 'USER'),
+(3, 'Test name3', 'Test password3', 'test@email3.com', 1, 'Test first name3', 'Test last name3', 3, 1, 'USER'),
+(4, 'Test name4', 'Test password4', 'test@email4.com', 1, 'Test first name4', 'Test last name4', 4, 1, 'USER'),
+(5, 'Test name5', 'Test password5', 'test@email5.com', 1, 'Test first name5', 'Test last name5', 5, 1, 'USER'),
+(6, 'Test name6', 'Test password6', 'test@email6.com', 1, 'Test first name6', 'Test last name6', 6, 1, 'USER');
