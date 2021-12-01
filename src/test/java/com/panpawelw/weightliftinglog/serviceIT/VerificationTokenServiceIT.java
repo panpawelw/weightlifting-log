4b05d9715d41398681df077dc38c079079983695
Paweł Wojtuń <panpawelw@pm.me>
2021-12-01 23:59:27 +0100

    Add VerificationTokenServiceIT

---
+++ src/test/java/com/panpawelw/weightliftinglog/serviceIT/VerificationTokenServiceIT.java
@@ -0,0 +1,27 @@
+package com.panpawelw.weightliftinglog.serviceIT;
+
+import com.panpawelw.weightliftinglog.models.User;
+import com.panpawelw.weightliftinglog.services.UserService;
+import com.panpawelw.weightliftinglog.services.VerificationTokenService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VerificationTokenServiceIT {
+
+  @Autowired
+  private VerificationTokenService service;
+
+  @Autowired
+  private UserService userService;
+
+  @Test
+  public void findByUserShouldSucceed() {
+    User testUser = userService.findUserByEmail("test@email1.com");
+    service.findByUser(testUser);
+  }
+}
