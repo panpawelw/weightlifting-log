bf0822f3c433fa974642a0a4f48916c40e3f0866
Paweł Wojtuń <panpawelw@pm.me>
2022-01-31 22:04:56 +0100

    Refactor WorkoutServiceIT

--- src/test/java/com/panpawelw/weightliftinglog/serviceIT/WorkoutServiceIT.java
+++ src/test/java/com/panpawelw/weightliftinglog/serviceIT/WorkoutServiceIT.java
@@ -28,21 +28,23 @@ public class WorkoutServiceIT {
 
   @Test
   public void findWorkoutsByUserSucceeds() {
-    System.out.println(service.findWorkoutById(1));
     List<WorkoutSerialized> workoutList =
         service.findWorkoutsByUser(userService.findUserByEmail("workout@test1.com"));
+
     assertEquals(3, workoutList.size());
   }
 
   @Test
   public void findWorkoutByIdSucceeds() {
-    WorkoutDeserialized testWorkout = service.findWorkoutById(1L);
+    WorkoutDeserialized testWorkout = service.findWorkoutById(1);
+
     assertEquals("Test workout 1 title", testWorkout.getTitle());
   }
 
   @Test
   public void findWorkoutByIdReturnsNull() {
-    WorkoutDeserialized testWorkout = service.findWorkoutById(5L);
+    WorkoutDeserialized testWorkout = service.findWorkoutById(100);
+
     assertNull(testWorkout);
   }
 
@@ -50,31 +52,38 @@ public class WorkoutServiceIT {
   public void saveWorkoutSucceeds() {
     long initialDatabaseCount = service.count();
     WorkoutDeserialized testWorkout = new WorkoutDeserialized(
-        0L, "Test workout 5 title", java.sql.Timestamp.valueOf("2022-01-01 07:10:10.0"),
+        0L, "Test save workout title", java.sql.Timestamp.valueOf("2022-01-01 07:10:10.0"),
         java.sql.Timestamp.valueOf("2022-01-01 08:10:10.0"),
         userService.findUserByEmail("workout@test2.com"),
         new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+
     service.saveWorkout(testWorkout);
+
     assertEquals(1, service.count() - initialDatabaseCount);
   }
 
   @Test
   public void deleteWorkoutSucceeds() {
     long initialDatabaseCount = service.count();
+
     service.deleteWorkout(4);
+
     assertEquals(1, initialDatabaseCount - service.count());
   }
 
   @Test(expected = EmptyResultDataAccessException.class)
   public void deleteWorkoutFails() {
     long initialDatabaseCount = service.count();
+
     service.deleteWorkout(100);
+
     assertEquals(0, initialDatabaseCount - service.count());
   }
 
   @Test
   public void serializeAndDeserializeWorkoutSucceed() {
     WorkoutDeserialized testWorkoutD = service.findWorkoutById(1);
+
     WorkoutSerialized testWorkoutS = service.serializeWorkout(testWorkoutD);
 
     assertEquals(testWorkoutD, service.deserializeWorkout(testWorkoutS));
