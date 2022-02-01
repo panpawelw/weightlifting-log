178178f707701d221412345ac8b69eae76564dfc
Paweł Wojtuń <panpawelw@pm.me>
2022-02-02 00:04:39 +0100

    Add getFileByWorkoutAndFilenameSucceeds()

--- src/test/java/com/panpawelw/weightliftinglog/serviceIT/DBFileServiceIT.java
+++ src/test/java/com/panpawelw/weightliftinglog/serviceIT/DBFileServiceIT.java
@@ -1,12 +1,16 @@
 package com.panpawelw.weightliftinglog.serviceIT;
 
-
+import com.panpawelw.weightliftinglog.models.MediaFile;
 import com.panpawelw.weightliftinglog.services.DBFileService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -22,7 +26,9 @@ public class DBFileServiceIT {
 
   @Test
   public void getFileByWorkoutAndFilenameSucceeds() {
+    MediaFile testFile = service.getFileByWorkoutIdAndFilename(5L, "testfile1.mp3");
 
+    assertArrayEquals(new byte[]{110, -96, 7, 47, 49, 24, 41, 113, 103, 123}, testFile.getContent());
   }
 
   @Test
